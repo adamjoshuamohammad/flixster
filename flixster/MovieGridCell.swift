98f4dae1fd12fdb66abc9d373df283a811ab2b0c
adamjoshuamohammad <73608454+adamjoshuamohammad@users.noreply.github.com>
2021-02-19 18:36:06 -0500

    Add files via upload

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Adam Joshua Mohammad on 2/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
